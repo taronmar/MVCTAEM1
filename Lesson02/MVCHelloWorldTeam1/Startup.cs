48ecb88bf528b82d6c50542f3ecd36b544e685c5
taronmar <taronmar@gmail.com>
2016-02-18 02:33:34 +0400

    Initial Project sources

---
+++ Lesson02/MVCHelloWorldTeam1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCHelloWorldTeam1.Startup))]
+namespace MVCHelloWorldTeam1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
