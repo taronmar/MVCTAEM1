45426f078530801047a8ff9222a49d79efa1d05c
taronmar <taronmar@gmail.com>
2016-02-20 04:26:50 +0400

    Warehouse

---
+++ Lesson02/MVCHelloWorldTeam1/Controllers/WarehouseController.cs
@@ -0,0 +1,34 @@
+﻿using MVCHelloWorldTeam1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHelloWorldTeam1.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        // GET: Warehouse
+        public ActionResult Index()
+        {
+
+            return View(new WarehouseAddProductModel());
+        }
+
+        
+        // POST: /Warehouse/AddProduct
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddProduct(WarehouseAddProductModel model, string returnUrl)
+        {
+            //TODO Saveinto the database
+            //TODO Refresh the page
+            RedirectToAction("Index", "Warehouse");
+
+            return View();
+        }
+    }
+}
---
+++ Lesson02/MVCHelloWorldTeam1/Models/WarehouseViewModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHelloWorldTeam1.Models
+{
+    public class WarehouseAddProductModel
+    {
+
+        public class Category
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public IEnumerable<SelectListItem> Categories
+        {
+           get{ return new SelectList(_categoryList, "Id", "Name"); }
+        }
+
+        public IEnumerable<Category> _categoryList =
+            new List<Category>
+            {
+                new Category {Id = 0, Name = "Drink"},
+                new Category {Id = 1, Name = "Chocolate"}
+            };
+
+        public string Name { get; set; }
+        public Int32 CategoryId { get; set; }
+        public Decimal Price { get; set; }
+        public Decimal Quantity { get; set; }
+        public Int32 Code { get; set; }
+    }
+}
